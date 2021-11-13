a5b772300ba561ad3b32f7c44edac2254f30081e
Do Di <dodi@Dos-MacBook-Air.local>
2021-11-13 23:31:22 +0700

    fourth commit

--- app/routes/category.routes.js
+++ app/routes/category.routes.js
@@ -1,11 +1,12 @@
 const category = require("../controllers/category.controller");
 let router = require("express").Router();
+const auth = require('../middleware/auth')
+
 module.exports = app => {
     
     router.get("/",category.getCategory);
 
-    router.post("/create",category.create);
-
+    router.post("/create",auth,category.create);
 
     app.use("/api/categories", router);
 }
